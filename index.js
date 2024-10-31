f3be2106c19c416b46086e3fbc853e01b278295a
dracocosmos <124890142+Dracocosmos@users.noreply.github.com>
2024-10-31 10:42:19 +0200

    all tests pass.

--- index.js
+++ index.js
@@ -8,25 +8,49 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// api endpoint 
+app.get("/api/:date?", function(req, res) {
+  let reqDate = req.params.date
+
+  dateO = reqDate == undefined
+    // if date parameter empty
+    ? new Date()
+    // if date is in a date format
+    : new Date(reqDate)
+
+  // if date is in unix format
+  dateO = isNaN(dateO)
+    ? new Date(+reqDate)
+    : dateO
+
+  // if date still invalid
+  dateO = isNaN(dateO)
+    ? res.json({ error: "Invalid Date" })
+    : dateO
+
+  // console.log(dateO)
+  const returnO = {
+    unix: Date.parse(dateO.toISOString()),
+    utc: dateO.toUTCString(),
+  }
+
+  res.json({ unix: returnO.unix, utc: returnO.utc });
 });
 
 
 
 // Listen on port set in environment variable or default to 3000
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT || 3000, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
